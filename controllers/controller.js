03438ce62f7054ec00985c6c7353623b9866c6f9
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2021-05-17 20:40:34 +0800

    removed uneccesary controllers

--- controllers/controller.js
+++ controllers/controller.js
@@ -21,44 +21,6 @@ const controller = {
 
     getTest: function(req, res) {
         res.render('test');
-    },
-
-    getCheckRefNo: function(req, res) {
-        var refno = req.query.refno;
-        
-        db.findOne(User, {refno: refno}, 'refno' ,function(result) {
-            res.send(result);
-        });
-    },
-
-
-    getAdd: function(req, res) {
-        var entry = {
-            name: req.query.name,
-            refno: req.query.refno,
-            amount: req.query.amount
-        }
-
-        db.insertOne(User, entry, function(flag) {
-            if(flag) {
-                //do nothing
-                res.send(entry);
-            }
-        });
-        
-    },
-
-
-    getDelete: function (req, res) {
-        // your code here
-        var conditions = {
-            refno: req.query.refno
-        }
-        db.deleteOne(User, conditions, function(flag) {
-            if(!flag) {
-                console.log('Uh oh, something went wrong...');
-            }
-        });
     }
 
 }
