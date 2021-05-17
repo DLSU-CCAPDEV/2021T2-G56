3e9515fa452a863a94d55cfabd4e48e5421e1f92
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2021-05-17 16:00:31 +0800

    fixed an error

--- controllers/controller.js
+++ controllers/controller.js
@@ -11,25 +11,6 @@ const controller = {
         res.render('index');
     },
 
-    getDB: function(req, res) {
-
-        db.findMany(User, {}, {}, {}, function(result) {
-            res.send({result});
-        });
-
-    },
-
-    getUser: function(req, res) {
-
-        var username = req.params.username;
-
-        db.findMany(User, { username:username }, {}, {}, function(result) {
-            res.send( {result} );
-        });
-
-
-    },
-
 }
 
 module.exports = controller;
--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,9 @@ const controller = require(`../controllers/controller.js`);
 
 const app = express();
 
+
 app.get(`/favicon.ico`, controller.getFavicon);
 app.get(`/`, controller.getIndex);
-app.get(`/getDB`, controller.getDB);
-app.get(`/getUser/:username`, controller.getUser);
+
 
 module.exports = app;
