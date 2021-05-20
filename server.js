ffff6666fa18d575f68955bdef245789e8019ce8
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2021-05-20 08:35:23 +0800

    added google drive api

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const dotenv = require(`dotenv`);
 const express = require(`express`);
 const session = require('express-session');
 const bodyParser = require(`body-parser`);
+const fileUpload = require('express-fileupload');
+const multer = require("multer");
+const { google } = require('googleapis');
+const path = require('path');
 const db = require(`./models/db.js`);
+const fs = require('fs');
 const ejs = require('ejs');
 
 
@@ -22,10 +27,83 @@ app.set('view engine', 'ejs');
 dotenv.config();
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
+const CLIENT_ID = '390227754248-tu672qpb5v9fc80u23t2048nro0m57kd.apps.googleusercontent.com';
+const CLIENT_SECRET = 'uRPVZow7FtKh3_Ks2yWW4uSF';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04SD-SlCNWjwlCgYIARAAGAQSNwF-L9IrqdtvtSNHwAGmKqpM-B_LqNBRoVIoE5gTxj_g2ZcV5VVJSRdkFcb4GmtMnGnDuqn-F_o';
+
+const oauth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+);
+
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const drive = google.drive({
+	version: 'v3',
+	auth: oauth2Client,
+});
+
+const filePath = './public/images/uploadedimg.png';
+const uploadsFolder = '13WR4NcjSdjgTff6EKTEdEpcCD7geYHvy';
+const profileimgFolder = '1VIJ7-Z5WOs4G7e1B7y5OTpQ8D9fPwrJc';
+const backgroundimgFolder = '1YhUG_W5XfZXgru7wt8Ik_Y5RcxwACNrj';
+
 
 app.use(express.static(`public`));
 
 db.connect();
+
+async function uploadFile(filename) {
+  try {
+    const response = await drive.files.create({
+      requestBody: {
+        name: Date.now() + ' -- ' + filename, //This can be name of your choice
+        mimeType: 'image/png',
+        parents: [folderId]
+      },
+      media: {
+		mimeType: 'image/png',
+        body: fs.createReadStream(filePath),
+      },
+    });
+
+    //makes picture PUBLIC
+    await drive.permissions.create({
+      fileId: response.data.id,
+      requestBody: {
+        role: 'reader',
+        type: 'anyone',
+      },
+    });
+
+	return response.data.id;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+
+var fileStorageEngine = multer.diskStorage({
+    destination: function (req,file,cb) {
+        cb(null,'./images');
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'tempimg.png');
+    }
+
+});
+
+const upload = multer ({ storage: fileStorageEngine });
+
+app.post('/single', upload.single('image'), function(req, res) {
+    uploadFile(req.file.originalname).then(x => {
+		console.log(x)
+    	res.send(req.file);
+	});
+});
+
 
 app.use(`/`, routes);
 
